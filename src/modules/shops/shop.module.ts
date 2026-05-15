cf1bcbf18f74a9e4d2332eb2989c12c74049bd60
Nguyễn Bá Anh Nguyên <baanhnguyenn@gmail.com>
2026-05-15 12:08:26 +0700

    Fix subcription purchase

--- src/modules/shops/shop.module.ts
+++ src/modules/shops/shop.module.ts
@@ -3,9 +3,9 @@ import { ShopService } from './shop.service';
 import { ShopController } from './shop.controller';
 import { CommonModule } from '../../common/common.module';
 import { DatabaseModule } from '../../database/database.module';
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
-  imports: [CommonModule, DatabaseModule],
+  imports: [CommonModule, DatabaseModule, AuthModule],
   controllers: [ShopController],
   providers: [ShopService],
   exports: [ShopService]
