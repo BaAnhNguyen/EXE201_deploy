6ba1e483e3a40a38c023abdacbd4210f2a83144e
Nguyễn Bá Anh Nguyên <baanhnguyenn@gmail.com>
2026-05-29 18:36:45 +0700

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: "http://localhost:5000",
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: [
+      'http://localhost:5000',
+      'https://lumio-frontend-inky.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,6 +22,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
